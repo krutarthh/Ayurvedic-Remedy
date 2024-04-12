f38a2b23b4636a2ef1b0666bff7c0b7078da7db6
Krutarth Patel <krupatel2807@gmail.com>
2024-04-12 17:33:18 -0400

    fixed api

---
+++ .history/app_20240412173249.js
@@ -0,0 +1,55 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+require('dotenv').config();
+// api call to get the model
+const API_KEY =  "not given for security reasons";
+
+console.log(API_KEY); 
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+
+// fetchign the elements
+const user_input = document.getElementById("user_input");
+const answer_area = document.getElementById("answer");
+const send_button = document.getElementById("Send");
+// redirect from enter action to the clicking the button.
+user_input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        send_button.click();
+    }
+});
+
+// on click event to get the answer from the model
+send_button.addEventListener("click", async () => {
+    answer_area.textContent = "";
+    const question = user_input.value;
+    const answer = await generateText(question + "give me a ayurvedic remedy, the answer should be short and specific, behave like a chatbot. if your response has a product i can buy give me the name and url to buy.");
+
+    const filtered_answer = answer.replace(/\*/g, '');
+    const typingSpeed = 28;
+    const answerText = filtered_answer;
+    let index = 0;
+    function typeAnswer() {
+        if (index < answerText.length) {
+            answer_area.textContent += answerText.charAt(index);
+            index++;
+            setTimeout(typeAnswer, typingSpeed);
+        }
+    }
+
+    typeAnswer(); 
+});
+
+// function to get the answer from the model
+async function generateText(prompt) {
+    
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+    return text;
+}
+
+
+
+      
